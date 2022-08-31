e34d6072cdc870133b48eae8e3795a978eaa25c6
Sepehr <sepehr@Sepehrs-MacBook-Pro.local>
2022-08-31 11:35:11 +0430

    add sidebar,navbar,index of admin panel

---
+++ panel/layouts/sidebar.php
@@ -0,0 +1,11 @@
+<section class="sidebar">
+    <section class="sidebar-link">
+        <a href="">panel</a>
+    </section>
+    <section class="sidebar-link">
+        <a href="">category</a>
+    </section>
+    <section class="sidebar-link">
+        <a href="">post</a>
+    </section>
+</section>
