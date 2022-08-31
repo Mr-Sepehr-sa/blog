6be11d1016103e9ef9acd050be1538c61e8423d1
Sepehr <sepehr@Sepehrs-MacBook-Pro.local>
2022-08-31 12:49:52 +0430

    build categories item and fix sidebar

--- panel/layouts/sidebar.php
+++ panel/layouts/sidebar.php
@@ -1,11 +1,11 @@
 <section class="sidebar">
     <section class="sidebar-link">
-        <a href="">panel</a>
+        <a href="<?= url('panel') ?>">panel</a>
     </section>
     <section class="sidebar-link">
-        <a href="">category</a>
+        <a href="<?= url('panel/category') ?>">category</a>
     </section>
     <section class="sidebar-link">
-        <a href="">post</a>
+        <a href="<?= url('panel/post') ?>">post</a>
     </section>
 </section>
