8a9e2923bdd0d87a1225aaede9eec232fa1e7dec
Sepehr <sepehr@Sepehrs-MacBook-Pro.local>
2022-08-31 14:50:51 +0430

    fix post index in panel

---
+++ panel/post/edit.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP panel</title>
+    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" media="all" type="text/css">
+    <link rel="stylesheet" href="../../assets/css/style.css" media="all" type="text/css">
+</head>
+
+<body>
+    <section id="app">
+
+
+        <section class="container-fluid">
+            <section class="row">
+                <section class="col-md-2 p-0">
+                </section>
+                <section class="col-md-10 pt-3">
+
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <section class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" name="title" id="title" placeholder="title ..." value="w">
+                        </section>
+                        <section class="form-group">
+                            <label for="image">Image</label>
+                            <input type="file" class="form-control" name="image" id="image">
+                        </section>
+                        <section class="form-group">
+                            <label for="cat_id">Category</label>
+                            <select class="form-control" name="cat_id" id="cat_id">
+                        </select>
+                        </section>
+                        <section class="form-group">
+                            <label for="body">Body</label>
+                            <textarea class="form-control" name="body" id="body" rows="5" placeholder="body ...">sss</textarea>
+                        </section>
+                        <section class="form-group">
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </section>
+                    </form>
+
+                </section>
+            </section>
+        </section>
+
+    </section>
+
+    <script src="../../assets/js/jquery.min.js"></script>
+    <script src="../../assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
